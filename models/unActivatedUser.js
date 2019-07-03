56b14b252a5c46fc706d209b736e752da8125488
Vikram singh <vikramsingh15j@gmail.com>
2019-07-03 14:02:06 +0530

    trim remove

--- models/unActivatedUser.js
+++ models/unActivatedUser.js
@@ -2,16 +2,23 @@ var mongoose=require("mongoose");
 var passportLocalMongoose=require("passport-local-mongoose");
 
 var unActivatedUserSchema=new mongoose.Schema({
-    name:String,
-    username:{type:String,unique:true,required:true},
-    email:{type:String,unique:true,required:true},
-    phone:String,
-    address:String,
-    city:String,
-    country:String
-    
-    
-    
+   name:String,
+   username:{type:String,unique:true,required:true},
+   password:String,
+   email:{type:String,unique:true,required:true},
+   phone:String,
+   address:String,
+   city:String,
+   country:String,
+   resetPasswordToken:String,
+   resetPasswordExpires:Date,
+   isAdmin:{type:Boolean,default:false},
+   orderId:[{
+   
+           type:mongoose.Schema.Types.ObjectId,
+           ref:"order"
+   }],
+   cart:{}
 });
 
 unActivatedUserSchema.plugin(passportLocalMongoose);
