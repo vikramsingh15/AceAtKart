312da8125020f5f8337f01481e1d174d3d2b48b0
Vikram singh <vikramsingh15j@gmail.com>
2019-07-03 18:51:16 +0530

    change

--- app.js
+++ app.js
@@ -74,10 +74,10 @@ app.use(function(req,res,next){
    res.locals.title="AceAtKart";
    res.locals.description="AceAtKart is an Indian e-commerce website, vending gadget supplies. It strives to provide gadget supplies like headphones, customised USB cables and many more. Supplies are affordable and of good quality. Also, Imparting you hassle free delivery to your doorstep.";
    res.locals.image=null;
-   /*if(req.user){
+   if(req.user&&req.user.cart){
         res.locals.qty=req.user.cart.totalQty||0;
-    }*/
-    res.locals.qty=0
+    }
+
    next();
 });
 
